4bec9857e1a756df5ae9f34972cd76cd443ddd84
MD. Nur Islam <142172668+NurTheDev@users.noreply.github.com>
2025-11-17 12:38:07 +0600

    wip: working on auth.controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,8 @@
-const { asyncHandler } = require("../helpers/asyncHandler");
-const { success } = require("../helpers/apiResponse");
+const {asyncHandler} = require("../helpers/asyncHandler");
+const {success} = require("../helpers/apiResponse");
 const {CustomError} = require("../helpers/customError");
 
 exports.signUp = asyncHandler(async (req, res) => {
-  console.log("signUp");
+    // Implement sign-up logic here
+    console.log("working");
 })
